2e910bcee8003e6b8705da9573109edb105a48dc
brandfang520 <brandfang520@gmail.com>
2026-09-11 02:35:44 +0800

    Delete sitemap.js

--- sitemap.js
+++
@@ -1,8 +0,0 @@
-export default function sitemap() {
-  const base = "https://coinvoyu.com";
-  return [
-    { url: base, lastModified: new Date() },
-    ...["beginner","bitcoin","ethereum","security","exchange","web3"].map(slug => ({url:`${base}/category/${slug}`,lastModified:new Date()})),
-    ...["1","2","3","4"].map(id => ({url:`${base}/article/${id}`,lastModified:new Date()}))
-  ];
-}
